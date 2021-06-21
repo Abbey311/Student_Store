d92bfe2997a1a4532ec4f678b4a4d16e9aab87f8
Abbey311 <y.abbey2@gmail.com>
2021-06-20 21:17:14 -0400

    made it look better

--- student-store-ui/src/components/ProductDescription/ProductDescription.js
+++ student-store-ui/src/components/ProductDescription/ProductDescription.js
@@ -59,7 +59,7 @@ export default function ProductDescription() {
             </div>
 
             <div className="price">
-            <>{products.price}</>
+            <>{"$" + products.price}</>
             </div>
         </div>
                 </>
